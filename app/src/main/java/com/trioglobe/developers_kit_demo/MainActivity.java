beb6f3805685f0b2069123de5409690c5f7660a5
AppMenu2 <appmenu2@gmail.com>
2020-11-07 14:38:41 +0530

    c

---
+++ app/src/main/java/com/trioglobe/developers_kit_demo/MainActivity.java
@@ -0,0 +1,81 @@
+package com.trioglobe.developers_kit_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.NativeExpressAdView;
+import com.google.android.gms.ads.VideoController;
+import com.google.android.gms.ads.VideoOptions;
+import com.google.android.gms.ads.formats.NativeAdOptions;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout relative;
+
+        private InterstitialAd mInterstitialAd;
+        Button show;
+    private static String LOG_TAG = "EXAMPLE";
+    NativeExpressAdView mAdView;
+    VideoController mVideoController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        relative = findViewById(R.id.relative);
+        mAdView = (NativeExpressAdView) findViewById(R.id.adView);
+
+// Set its video options.
+        mAdView.setVideoOptions(new VideoOptions.Builder()
+                .setStartMuted(true)
+                .build());
+
+// The VideoController can be used to get lifecycle events and info about an ad's video
+// asset. One will always be returned by getVideoController, even if the ad has no video
+// asset.
+        mVideoController = mAdView.getVideoController();
+        mVideoController.setVideoLifecycleCallbacks(new VideoController.VideoLifecycleCallbacks() {
+            @Override
+            public void onVideoEnd() {
+                Log.d(LOG_TAG, "Video playback is finished.");
+                super.onVideoEnd();
+            }
+        });
+
+// Set an AdListener for the AdView, so the Activity can take action when an ad has finished
+// loading.
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                if (mVideoController.hasVideoContent()) {
+                    Log.d(LOG_TAG, "Received an ad that contains a video asset.");
+                } else {
+                    Log.d(LOG_TAG, "Received an ad that does not contain a video asset.");
+                }
+            }
+        });
+
+        mAdView.loadAd(new AdRequest.Builder().build());
+
+    }
+
+}
